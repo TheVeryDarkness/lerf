9645397cf67b6b89d9d6ccce7249fb2bc88f3cfa
TheVeryDarkness <3266343194@qq.com>
2024-01-15 20:33:17 +0800

    Added lerf-siglip

--- lerf/lerf_config.py
+++ lerf/lerf_config.py
@@ -165,3 +165,56 @@ lerf_method_lite = MethodSpecification(
     ),
     description="A lightweight version of LERF designed to work on smaller GPUs",
 )
+
+
+lerf_method_siglip = MethodSpecification(
+    config=TrainerConfig(
+        method_name="lerf-siglip",
+        steps_per_eval_batch=500,
+        steps_per_save=2000,
+        max_num_iterations=30000,
+        mixed_precision=True,
+        pipeline=LERFPipelineConfig(
+            datamanager=LERFDataManagerConfig(
+                dataparser=NerfstudioDataParserConfig(train_split_fraction=0.99),
+                train_num_rays_per_batch=4096,
+                eval_num_rays_per_batch=4096,
+                camera_optimizer=CameraOptimizerConfig(
+                    mode="SO3xR3", optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2)
+                ),
+            ),
+            model=LERFModelConfig(
+                eval_num_rays_per_chunk=1 << 15,
+                # NOTE: exceeding 16 layers per hashgrid causes a segfault within Tiny CUDA NN, so instead we compose multiple hashgrids together
+                hashgrid_sizes=(19, 19),
+                hashgrid_layers=(12, 12),
+                hashgrid_resolutions=((16, 128), (128, 512)),
+                num_lerf_samples=24,
+            ),
+            network=OpenCLIPNetworkConfig(
+                clip_model_type="ViT-B-16-SigLIP-384", clip_model_pretrained="webli", clip_n_dims=512
+            ),
+            #  You can swap the type of input encoder by specifying different NetworkConfigs, the one below uses OpenAI CLIP, the one above uses OpenCLIP
+            # network=CLIPNetworkConfig(
+            #     clip_model_type="ViT-B/16", clip_n_dims=512
+            # )
+        ),
+        optimizers={
+            "proposal_networks": {
+                "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": None,
+            },
+            "fields": {
+                "optimizer": RAdamOptimizerConfig(lr=1e-2, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-3, max_steps=30000),
+            },
+            "lerf": {
+                "optimizer": RAdamOptimizerConfig(lr=1e-2, eps=1e-15, weight_decay=1e-9),
+                "scheduler": ExponentialDecaySchedulerConfig(lr_final=1e-3, max_steps=4000),
+            },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+        vis="viewer",
+    ),
+    description="An experimental version of LERF with SigLIP",
+)
